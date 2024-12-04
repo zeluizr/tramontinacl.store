4f01c841ef522ed44860e9a05733d3d6162e0878
Jose Luiz Rodrigues <zeluizr@me.com>
2024-12-03 22:51:16 -0300

    remove ListAnchor component and related styles; delete unused translations and update manifest

--- react/helpers/TypographyMap.js
+++
@@ -1,5 +0,0 @@
-export const TypographyMap = {
-	body: "t-body",
-	mini: "t-mini",
-	small: "t-small",
-};
--- react/utils/message.js
+++
@@ -1,8 +0,0 @@
-import { defineMessages } from "react-intl";
-
-export const messages = defineMessages({
-	title: {
-		id: "admin/editor.list-anchor.title",
-		defaultMessage: "",
-	},
-});
