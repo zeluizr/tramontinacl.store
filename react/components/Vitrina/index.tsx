59f8905fe9ae0ade62aa2720e525f47e0a9400de
Jose Luiz Rodrigues <zeluizr@me.com>
2025-05-28 15:41:00 -0400

    feat: refatorar componente Vitrina para melhorar a renderização; adicionar suporte para Vitrina #03 e atualizar configurações em _vitrinas.jsonc e home.jsonc

--- react/components/Vitrina/index.tsx
+++ react/components/Vitrina/index.tsx
@@ -15,32 +15,38 @@ const CSS_HANDLES = [
 ] as const;
 function Vitrina({ title, backgroundImage, subTitle, children }: VitrinaProps) {
 	const { handles } = useCssHandles(CSS_HANDLES);
-	const _renderer = useMemo(() => renderer({ isWhite: backgroundImage ? true : false }), []);
+	const isWhite = !!backgroundImage || (!!title && !!subTitle);
+
+	const _renderer = useMemo(() => {
+		return renderer({ isWhite });
+	}, [backgroundImage, title, subTitle]);
 
 	return (
 		<section
 			className={classNames(handles.vitrinaContainer, backgroundImage ? "br3 pa5" : "br0 pa0")}
 			style={backgroundImage ? { backgroundImage: `url(${backgroundImage})` } : undefined}
 		>
-			{title && (
-				<div
-					className={classNames(
-						backgroundImage ? handles.vitrinaWhiteTitle : handles.vitrinaTitle,
-						"flex justify-center items-center"
-					)}
-					dangerouslySetInnerHTML={{
-						__html: title && _renderer ? marked.parse(title, { renderer: _renderer }) : "",
-					}}
-				/>
-			)}
-			{subTitle && (
-				<div
-					className={classNames(handles.vitrinaSubTitle, "flex justify-center items-center")}
-					dangerouslySetInnerHTML={{
-						__html: subTitle && _renderer ? marked.parse(subTitle, { renderer: _renderer }) : "",
-					}}
-				/>
-			)}
+			<div className={classNames({ "bg-action-primary br3 pa5": !backgroundImage && !!title && !!subTitle })}>
+				{title && (
+					<div
+						className={classNames(
+							isWhite ? handles.vitrinaWhiteTitle : handles.vitrinaTitle,
+							"flex justify-center items-center"
+						)}
+						dangerouslySetInnerHTML={{
+							__html: title && _renderer ? marked.parse(title, { renderer: _renderer }) : "",
+						}}
+					/>
+				)}
+				{subTitle && (
+					<div
+						className={classNames(handles.vitrinaSubTitle, "flex justify-center items-center")}
+						dangerouslySetInnerHTML={{
+							__html: subTitle && _renderer ? marked.parse(subTitle, { renderer: _renderer }) : "",
+						}}
+					/>
+				)}
+			</div>
 			<div className={classNames(handles.vitrinaShelfContainer, "mt5")}>{children}</div>
 		</section>
 	);
